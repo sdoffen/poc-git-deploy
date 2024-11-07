8a53b95d77d15084880867f53d54eadf73c44e90
Scott Offen <118468886+sdoffen@users.noreply.github.com>
2024-11-07 00:22:02 -0700

    Remove readme

--- src/GitHub.Client/ClientMarker.cs
+++ src/GitHub.Client/ClientMarker.cs
@@ -2,8 +2,8 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace GitHub.Client;
 
-[ExcludeFromCodeCoverage]
 /// <summary>
 /// A marker class to ensure the client is only registered once.
 /// </summary>
+[ExcludeFromCodeCoverage]
 internal sealed class ClientMarker { /* This class is intentionally empty */ }
